f3721811ae601ea984c13992e857f0de4ee8b7e5
user <user@use1r-PC>
2017-01-22 16:32:31 +0800

    Initial Commit

---
+++ JQuery/src/main/webapp/MyJquery.js
@@ -0,0 +1,5 @@
+// It is okay to write jquery codes directly into the external js.
+// By defining the external js file, different HTML fils can share the same js.
+$(document).ready(function() {
+	alert("Hello World");
+});
